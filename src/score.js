5edb0e7f45cc5e4968a485e10d4fb53f733de9d0
Tushar <singhtushar348@gmail.com>
2022-07-20 15:47:38 +0100

    modify score.js file with addscore and scoreboard function

--- src/score.js
+++ src/score.js
@@ -1,15 +1,27 @@
+import { saveUser, getUser } from "./leaderboard.js"
+
 const scoreList = document.querySelector('.score-list');
 const inputName = document.querySelector('#name');
 const inputScore = document.querySelector('#score');
 const submitBtn = document.querySelector('.submit');
+const refreshBtn = document.querySelector('.refresh');
 
-function addScore() {
+const scoreBoard = async () => {
+  let users = await getUser();
+  users.result.sort(function(a,b){return a.score-b.score});
+  scoreList.innerText = '';
+  users.result.forEach((user) => {
+    const score = document.createElement('li');
+    score.innerText = `${user.user} : ${user.score}`;
+    scoreList.appendChild(score);
+  })
+};
+
+const addScore = async () => {
   if (inputName.value === '' || inputScore.value === '') return;
-  const score = document.createElement('li');
-  score.innerText = `${inputName.value} : ${inputScore.value}`;
-  scoreList.appendChild(score);
-}
+  saveUser();
+};
 
 export {
-  addScore, scoreList, inputName, inputScore, submitBtn,
+  addScore, scoreBoard, inputName, inputScore, submitBtn, scoreList, refreshBtn
 };
